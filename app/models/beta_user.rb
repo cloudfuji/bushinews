6db607f7a75b8b42f5e73a14eede6837260e577e
Kev Zettler <kevzettler@gmail.com>
2011-06-14 13:38:30 -0700

    removed beta users

--- app/models/beta_user.rb
+++
@@ -1,6 +0,0 @@
-class BetaUser < ActiveRecord::Base
-  before_save do 
-    puts "BETA USER AFTER SAVE"
-    return Bushido::Data.publish(:customer_lead,self)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 EngineTest::Application.routes.draw do
+  resources :bushidomessages
+
   resources :users
 
   resources :beta_users
@@ -52,7 +54,7 @@ EngineTest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "application#index"
+  root :to => "bushidomessages#index"
 
   # See how all your routes lay out with "rake routes"
 
