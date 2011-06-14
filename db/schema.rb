755bd260cfe49c053525aa4b571a63f501e14d8e
Kev Zettler <kevzettler@gmail.com>
2011-06-14 13:59:48 -0700

    added refresh to appmessages index page git push origin master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110613135858) do
+ActiveRecord::Schema.define(:version => 20110614203537) do
 
   create_table "beta_users", :force => true do |t|
     t.string   "first_name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(:version => 20110613135858) do
     t.string   "email"
     t.string   "bushido_data_id"
     t.string   "bushido_version"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bushidomessages", :force => true do |t|
+    t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
