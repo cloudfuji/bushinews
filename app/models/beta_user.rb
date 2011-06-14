e4d8365ea39f0b1171f79234a77afa7179ab5c23
Kev Zettler <kevzettler@gmail.com>
2011-06-13 23:18:50 -0700

    added return to before_save callback

--- app/models/beta_user.rb
+++ app/models/beta_user.rb
@@ -1,6 +1,6 @@
 class BetaUser < ActiveRecord::Base
-  after_save do 
+  before_save do 
     puts "BETA USER AFTER SAVE"
-    Bushido::Data.publish(:customer_lead,self)
+    return Bushido::Data.publish(:customer_lead,self)
   end
 end
