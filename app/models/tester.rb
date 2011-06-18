809a27fc9f19de841534b541c2046badb9a1dac8
Kev Zettler <kevzettler@gmail.com>
2011-06-18 12:01:50 -0700

    debugging

--- app/models/tester.rb
+++ app/models/tester.rb
@@ -1,6 +1,6 @@
 class Tester < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  bushido :customer_lead
+  #bushido :customer_lead
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :bushido_id, :bushido_version
